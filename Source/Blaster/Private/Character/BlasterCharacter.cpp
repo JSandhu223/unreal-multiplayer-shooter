11938cff1829594d31d085a5060bc42f98973044
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2025-11-12 17:56:30 -0700

    Configured input action mappings on 'BlasterCharacter'. Utilized the existing input actions and input mapping contexts that were provided from the third person blueprint template. The blaster character can now jump, move with WASD, and use the mouse to look around.

--- Source/Blaster/Private/Character/BlasterCharacter.cpp
+++ Source/Blaster/Private/Character/BlasterCharacter.cpp
@@ -4,6 +4,11 @@
 #include "Character/BlasterCharacter.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "GameFramework/PlayerController.h"
 
 
 ABlasterCharacter::ABlasterCharacter()
@@ -24,6 +29,16 @@ ABlasterCharacter::ABlasterCharacter()
 void ABlasterCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	// TODO: move enhanced input code to a player controller class
+	check(KeyboardContext);
+	check(MouseContext);
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetController<APlayerController>()->GetLocalPlayer());
+	if (Subsystem)
+	{
+		Subsystem->AddMappingContext(KeyboardContext, 0);
+		Subsystem->AddMappingContext(MouseContext, 0);
+	}
 	
 }
 
@@ -37,5 +52,50 @@ void ABlasterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	// TODO: Move enhanced input code to a player controller class
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ABlasterCharacter::DoJump);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABlasterCharacter::DoMove);
+	EnhancedInputComponent->BindAction(MouseLookAction, ETriggerEvent::Triggered, this, &ABlasterCharacter::DoMouseLook);
 }
 
+void ABlasterCharacter::DoJump(const FInputActionValue& InputActionValue)
+{
+	float Value = InputActionValue.Get<float>();
+
+	//UE_LOG(LogTemp, Warning, TEXT("Jump value: %f"), Value);
+
+	// For now, we'll just call the existing Jump function from the Character class
+	Jump();
+}
+
+void ABlasterCharacter::DoMove(const FInputActionValue& InputActionValue)
+{
+	FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+
+	//UE_LOG(LogTemp, Warning, TEXT("Jump value: %s"), *InputAxisVector.ToString());
+
+	if (GetController() != nullptr)
+	{
+		const FRotator YawRotation(0.0f, GetController()->GetControlRotation().Yaw, 0.0f);
+		const FVector ForwardDirection(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X));
+		const FVector RightDirection(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y));
+
+		AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
+
+void ABlasterCharacter::DoMouseLook(const FInputActionValue& InputActionValue)
+{
+	FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+
+	//UE_LOG(LogTemp, Warning, TEXT("Jump value: %s"), *InputAxisVector.ToString());
+
+	if (GetController() != nullptr)
+	{
+		AddControllerYawInput(InputAxisVector.X);
+		AddControllerPitchInput(InputAxisVector.Y);
+	}
+}
--- Source/Blaster/Public/Character/BlasterCharacter.h
+++ Source/Blaster/Public/Character/BlasterCharacter.h
@@ -22,10 +22,31 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	void DoJump(const struct FInputActionValue& InputActionValue);
+
+	void DoMove(const FInputActionValue& InputActionValue);
+
+	void DoMouseLook(const FInputActionValue& InputActionValue);
+
 private:
 	UPROPERTY(VisibleAnywhere, Category = "Camera")
 	class USpringArmComponent* CameraBoom;
 	
 	UPROPERTY(VisibleAnywhere, Category = "Camera")
 	class UCameraComponent* FollowCamera;
+
+	UPROPERTY(EditAnywhere, Category = "Enhanced Input|Mapping Contexts")
+	class UInputMappingContext* KeyboardContext;
+
+	UPROPERTY(EditAnywhere, Category = "Enhanced Input|Mapping Contexts")
+	class UInputMappingContext* MouseContext;
+
+	UPROPERTY(EditAnywhere, Category = "Enhanced Input|Input Actions")
+	class UInputAction* JumpAction;
+
+	UPROPERTY(EditAnywhere, Category = "Enhanced Input|Input Actions")
+	class UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, Category = "Enhanced Input|Input Actions")
+	class UInputAction* MouseLookAction;
 };
