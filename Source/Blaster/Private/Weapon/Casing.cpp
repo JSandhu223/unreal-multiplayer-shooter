a3e1eb4606d468afc52ef56e0eae1951528116fa
Jasraj Sandhu <jasrajsandhu01@gmail.com>
2025-12-10 20:39:04 -0700

    Added physics and sound cue to the casing. The casing now gets destroyed via OnHit(). Imported several sound files for shells, which get played at random in our custom sound cue.

--- Source/Blaster/Private/Weapon/Casing.cpp
+++ Source/Blaster/Private/Weapon/Casing.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Weapon/Casing.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
 
 
 ACasing::ACasing()
@@ -10,11 +12,28 @@ ACasing::ACasing()
 
 	this->CasingMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CasingMesh"));
 	SetRootComponent(this->CasingMesh);
+	this->CasingMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+	this->CasingMesh->SetSimulatePhysics(true);
+	this->CasingMesh->SetEnableGravity(true);
+	this->CasingMesh->SetNotifyRigidBodyCollision(true);
 
+	this->ShellEjectionImpulse = 10.0f;
 }
 
 void ACasing::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	this->CasingMesh->OnComponentHit.AddDynamic(this, &ACasing::OnHit);
+	this->CasingMesh->AddImpulse(this->GetActorForwardVector() * this->ShellEjectionImpulse);
+}
+
+void ACasing::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (this->ShellSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, this->ShellSound, this->GetActorLocation());
+	}
+
+	this->Destroy();
 }
